af9a6784d4acf634a248b684a90745e2d1848d79
Đặng Thùy Trang <83824467+dangthuytrang@users.noreply.github.com>
2021-06-30 22:19:59 +0700

    hw-dome

--- hw.js
+++ hw.js
@@ -1,34 +1,6 @@
-let stuff = ["adidas", "gucci", "converse", "chanel"];
-let option = prompt("What do you want? (C, R, U, D)").toLocaleUpperCase();
-if (option === "C") {
-    let new_item = prompt("Enter your new stuff");
-    stuff.push(new_item)
-}
-else if (option === "D") {
-    let del = Number(prompt("Enter the index of stuff you want to delete : "));
-    if (del < 0 || del >= stuff.length) {
-        alert("Please other index :");
-    } else {
-        stuff.splice(del, 1);
-    }
-}
-
-else if (option === "U") {
-    let update = Number(prompt("Enter the index of stuff : "));
-    if (update < 0 || update >= stuff.length) {
-        alert("Please other index :");
-    } else {
-        let valueUpdate = (prompt("Enter value to update :"));
-        stuff[update] = valueUpdate;
-    }
-}
-else if (option === "R") {
-    let read = Number(prompt("Enter the index of stuff : "));
-    console.log(stuff[read]);
-}
-else {
-    prompt("What do you want? (C, R, U, D)").toLocaleUpperCase();
-}
-for (let i = 0; i < stuff.length; i++) {
-    console.log(i, stuff[i]);
-}
+let submit = document.getElementById("input");
+let btn = document.querySelector("button");
+let text = document.querySelector("h2")
+submit.addEventListener("input", ()=>{
+    text.innerText = submit.value;
+})
